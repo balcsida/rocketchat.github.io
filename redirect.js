438cd361619b163a8f306296559dc72d952921b2
Martin Schoeler <martin.schoeler@rocket.chat>
2018-09-05 17:24:22 -0300

    test javascript redirect

--- redirect.js
+++ redirect.js
@@ -3,3 +3,9 @@
 if(location.hostname == "rocketchat.github.io" && location.href.indexOf('?noredirect') == -1) {
 	location="https://rocket.chat" + location.pathname
 }
+
+// remove trailing slashes from links
+var url = location.href;
+if(url.substr(url.length - 1) === '/') {
+	window.location = url.substr(0, url.length - 1);
+}
